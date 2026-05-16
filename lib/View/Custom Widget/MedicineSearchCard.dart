cfc5d28d9bde482b1b4dd963b85fa38d7d50c72f
razak <rm7740736@gmail.com>
2026-05-16 18:38:19 +0500

    complete

--- lib/View/Custom Widget/MedicineSearchCard.dart
+++ lib/View/Custom Widget/MedicineSearchCard.dart
@@ -4,17 +4,18 @@ import 'package:pharmacymanagement/View/Custom%20Widget/CustomText.dart';
 class Medicinesearchcard extends StatefulWidget {
   final String Name;
   final String Status;
-  final double  price;
+  final double price;
   final String ExpiryDate;
   final int Stock;
-  const Medicinesearchcard({super.key,
+  const Medicinesearchcard({
+    super.key,
 
-
-  required this.Name,
+    required this.Name,
     required this.price,
     required this.Status,
-  required this.ExpiryDate,
-  required this.Stock});
+    required this.ExpiryDate,
+    required this.Stock,
+  });
 
   @override
   State<Medicinesearchcard> createState() => _MedicinesearchcardState();
@@ -24,45 +25,39 @@ class _MedicinesearchcardState extends State<Medicinesearchcard> {
   @override
   Widget build(BuildContext context) {
     return Container(
-margin: EdgeInsetsGeometry.all(5),
+      margin: EdgeInsetsGeometry.all(5),
       padding: EdgeInsets.all(10),
       width: double.infinity,
       decoration: BoxDecoration(
         color: Colors.white70,
         borderRadius: BorderRadius.circular(8),
         boxShadow: [
-          BoxShadow(
-            offset: Offset(2, 2),
-            blurRadius: 4,
-            color: Colors.black,
-          ),
+          BoxShadow(offset: Offset(2, 2), blurRadius: 4, color: Colors.black),
         ],
-
       ),
       child: Row(
-
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Column(crossAxisAlignment: CrossAxisAlignment.start,
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              CustomText(text: "Name",color: Colors.blueAccent,),
-              CustomText(text: "Price",color: Colors.blueAccent,),
-              CustomText(text: "Stock",color: Colors.blueAccent,),
-              CustomText(text: "Expiry",color: Colors.blueAccent,),
-
+              CustomText(text: "Name", color: Colors.blueAccent),
+              CustomText(text: "Price", color: Colors.blueAccent),
+              CustomText(text: "Stock", color: Colors.blueAccent),
+              CustomText(text: "Expiry", color: Colors.blueAccent),
             ],
           ),
-          SizedBox(width: 20,),
-          Column(crossAxisAlignment: CrossAxisAlignment.start,
+          SizedBox(width: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               CustomText(text: widget.Name),
               CustomText(text: widget.price.toString()),
               CustomText(text: widget.Stock.toString()),
               CustomText(text: widget.ExpiryDate),
-
             ],
           ),
-Spacer(),
+          Spacer(),
           Container(
             padding: EdgeInsets.all(6),
             decoration: BoxDecoration(
@@ -75,12 +70,6 @@ Spacer(),
             ),
             child: CustomText(text: widget.Status, fontSize: 10),
           ),
-
-
-
-
-
-
         ],
       ),
     );
