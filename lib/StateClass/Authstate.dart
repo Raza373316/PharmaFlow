95d9ff94a073c22f561afafe447cb8412cd63a03
razak <rm7740736@gmail.com>
2026-06-27 09:58:30 +0500

    auth sate

---
+++ lib/StateClass/Authstate.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthState {
+  final User? user;
+  final bool isLoading;
+  final String? error;
+  final bool isLoggedIn;
+
+  AuthState({
+    this.user,
+    this.isLoading = false,
+    this.error,
+    this.isLoggedIn = false,
+  });
+
+  AuthState copyWith({
+    User? user,
+
+    bool? isLoading,
+    String? error,
+    bool? isLoggedIn,
+  }) {
+    return AuthState(
+      user: user?? this.user,
+      isLoading: isLoading ?? this.isLoading,
+      error: error,
+      isLoggedIn: isLoggedIn ?? this.isLoggedIn,
+    );
+  }
+}
