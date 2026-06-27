c604358e594b4be9da5edd4a034c05f856268885
razak <rm7740736@gmail.com>
2026-06-27 09:55:39 +0500

    done

--- lib/View/AppScreens/Addmedicine.dart
+++ lib/View/AppScreens/Addmedicine.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pharmacymanagement/View/Custom%20Widget/CustomButton.dart';
 import 'package:pharmacymanagement/View/Custom%20Widget/CustomText.dart';
 import 'package:pharmacymanagement/View/Custom%20Widget/CustomTextField.dart';
 
-class Addmedicine extends StatefulWidget {
+import '../../Modal/MedicineModel.dart';
+import '../../Provider/MedicineProvider.dart';
+
+class Addmedicine extends ConsumerStatefulWidget {
   const Addmedicine({super.key});
 
   @override
-  State<Addmedicine> createState() => _AddmedicineState();
+  ConsumerState<Addmedicine> createState() => _AddmedicineState();
 }
 
-class _AddmedicineState extends State<Addmedicine> {
+class _AddmedicineState extends ConsumerState<Addmedicine> {
   TextEditingController medicinename = TextEditingController();
   TextEditingController catergory = TextEditingController();
   TextEditingController company = TextEditingController();
@@ -20,6 +24,7 @@ class _AddmedicineState extends State<Addmedicine> {
   TextEditingController expirydate = TextEditingController();
   @override
   Widget build(BuildContext context) {
+    final medicineState = ref.watch(medicineProvider);
     return Scaffold(
       appBar: AppBar(
         title: CustomText(text: "Add Medicine", fontWeight: FontWeight.bold),
@@ -33,7 +38,7 @@ class _AddmedicineState extends State<Addmedicine> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               CustomText(text: "Add Medicine", color: Colors.white),
-        
+
               SizedBox(height: 5),
               CustomTextField(
                 controller: medicinename,
@@ -41,55 +46,72 @@ class _AddmedicineState extends State<Addmedicine> {
               ),
               SizedBox(height: 10),
               CustomText(text: "Category", color: Colors.white),
-        
+
               SizedBox(height: 5),
-              CustomTextField(
-                controller: catergory,
-                hintText: "Medicine Name",
-              ),
+              CustomTextField(controller: catergory, hintText: "Medicine Name"),
               SizedBox(height: 10),
               CustomText(text: "Company", color: Colors.white),
-        
+
               SizedBox(height: 5),
-              CustomTextField(
-                controller: company,
-                hintText: "Company",
-              ),
+              CustomTextField(controller: company, hintText: "Company"),
               SizedBox(height: 10),
               CustomText(text: "Purchase Price", color: Colors.white),
-        
+
               SizedBox(height: 5),
-              CustomTextField(
-                controller: purchaseprice,
-                hintText: "100Pkr",
-              ),
+              CustomTextField(controller: purchaseprice, hintText: "100Pkr"),
               SizedBox(height: 10),
               CustomText(text: "Selling Price", color: Colors.white),
-        
+
               SizedBox(height: 5),
-              CustomTextField(
-                controller: sellingprice,
-                hintText: "120",
-              ),
+              CustomTextField(controller: sellingprice, hintText: "120"),
               SizedBox(height: 10),
               CustomText(text: "Stock Quantity", color: Colors.white),
-        
+
               SizedBox(height: 5),
-              CustomTextField(
-                controller:stockquantity,
-                hintText: "50",
-              ),
+              CustomTextField(controller: stockquantity, hintText: "50"),
               SizedBox(height: 10),
               CustomText(text: "Expiry Date", color: Colors.white),
-        
+
               SizedBox(height: 5),
-              CustomTextField(
-                controller:expirydate,
-                hintText: "dd/mm/yyyy",
+              CustomTextField(controller: expirydate, hintText: "dd/mm/yyyy"),
+              SizedBox(height: 20),
+              CustomButton(
+                text: 'Save Medicine',
+                height: 40,
+                isLoading: medicineState.isLoading,
+                onPressed: () async {
+                  if (medicinename.text.isEmpty ||
+                      stockquantity.text.isEmpty ||
+                      purchaseprice.text.isEmpty ||
+                      company.text.isEmpty ||
+                      expirydate.text.isEmpty ||
+                      sellingprice.text.isEmpty ||
+                      catergory.text.isEmpty) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text("Please fill all fields")),
+                    );
+                    return;
+                  }
+                  final medicineId = DateTime.now().millisecondsSinceEpoch
+                      .toString();
+
+                  final medicine = MedicineModel(
+                    id: medicineId,
+                    name: medicinename.text.trim(),
+                    stock: int.parse(stockquantity.text.trim()),
+                    purchasePrice: double.parse(purchaseprice.text.trim()),
+                    sellPrice: double.parse(sellingprice.text.trim()),
+                    category: catergory.text.trim(),
+                    company: company.text.trim(),
+                    expiryDate: expirydate.text.trim(),
+                  );
+
+                  await ref
+                      .read(medicineProvider.notifier)
+                      .addMedicine(medicine);
+                },
               ),
-              SizedBox(height: 20,),
-              CustomButton(text: 'Save Medicine',height: 40, onPressed: (){}),
-              SizedBox(height: 50,),
+              SizedBox(height: 50),
             ],
           ),
         ),
