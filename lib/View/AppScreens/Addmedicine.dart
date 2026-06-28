fc3eae54bde0eb7628635a9f4c5b94f9a34b4d97
razak <rm7740736@gmail.com>
2026-06-28 14:14:47 +0500

    done

--- lib/View/AppScreens/Addmedicine.dart
+++ lib/View/AppScreens/Addmedicine.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pharmacymanagement/View/AppScreens/MainScreenNavigation.dart';
+import 'package:pharmacymanagement/View/AppScreens/Search%20Medicine.dart';
 import 'package:pharmacymanagement/View/Custom%20Widget/CustomButton.dart';
 import 'package:pharmacymanagement/View/Custom%20Widget/CustomText.dart';
 import 'package:pharmacymanagement/View/Custom%20Widget/CustomTextField.dart';
@@ -22,7 +25,32 @@ class _AddmedicineState extends ConsumerState<Addmedicine> {
   TextEditingController sellingprice = TextEditingController();
   TextEditingController stockquantity = TextEditingController();
   TextEditingController expirydate = TextEditingController();
-  @override
+  final List<String> categories = [
+    "Tablet",
+    "Syrup",
+    "Capsule",
+    "Injection",
+    "Spray",
+  ];
+  String? selectedCategory;
+
+
+  Future<void> _selectDate(TextEditingController controller) async {
+    DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime.now(),
+      lastDate: DateTime(DateTime.now().year+7),
+    );
+
+    if (pickedDate != null) {
+      controller.text =
+      "${pickedDate.day}/${pickedDate.month}/${pickedDate.year}";
+    }
+  }
+
+    @override
+
   Widget build(BuildContext context) {
     final medicineState = ref.watch(medicineProvider);
     return Scaffold(
@@ -48,8 +76,60 @@ class _AddmedicineState extends ConsumerState<Addmedicine> {
               CustomText(text: "Category", color: Colors.white),
 
               SizedBox(height: 5),
-              CustomTextField(controller: catergory, hintText: "Medicine Name"),
-              SizedBox(height: 10),
+            DropdownButtonFormField<String>(
+              value: selectedCategory,
+              decoration: InputDecoration(
+                hintText: "Select Category",
+
+                filled: true,
+                fillColor: Colors.grey.shade100,
+
+                contentPadding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 18,
+                ),
+
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(14),
+                  borderSide: BorderSide.none,
+                ),
+
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(14),
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade300,
+                  ),
+                ),
+
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(14),
+                  borderSide: const BorderSide(
+                    color: Colors.blue,
+                    width: 2,
+                  ),
+                ),
+
+                errorBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(14),
+                  borderSide: const BorderSide(
+                    color: Colors.red,
+                    width: 1.5,
+                  ),
+                ),
+              ),
+              items: categories.map((category) {
+                return DropdownMenuItem(
+                  value: category,
+                  child: Text(category),
+                );
+              }).toList(),
+              onChanged: (value) {
+                setState(() {
+                  selectedCategory = value;
+                });
+              },
+            ),
+               SizedBox(height: 10),
               CustomText(text: "Company", color: Colors.white),
 
               SizedBox(height: 5),
@@ -58,22 +138,38 @@ class _AddmedicineState extends ConsumerState<Addmedicine> {
               CustomText(text: "Purchase Price", color: Colors.white),
 
               SizedBox(height: 5),
-              CustomTextField(controller: purchaseprice, hintText: "100Pkr"),
+              CustomTextField(controller: purchaseprice, hintText: "100Pkr",
+                inputFormatters: [
+                  FilteringTextInputFormatter.digitsOnly, // For integers (e.g., 10, 20)
+
+                ],),
               SizedBox(height: 10),
               CustomText(text: "Selling Price", color: Colors.white),
 
               SizedBox(height: 5),
-              CustomTextField(controller: sellingprice, hintText: "120"),
+              CustomTextField(controller: sellingprice, hintText: "120",
+                inputFormatters: [
+                  FilteringTextInputFormatter.digitsOnly, // For integers (e.g., 10, 20)
+
+                ],
+              ),
               SizedBox(height: 10),
               CustomText(text: "Stock Quantity", color: Colors.white),
 
               SizedBox(height: 5),
-              CustomTextField(controller: stockquantity, hintText: "50"),
+              CustomTextField(controller: stockquantity, hintText: "50",
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly, // For integers (e.g., 10, 20)
+
+              ],),
               SizedBox(height: 10),
               CustomText(text: "Expiry Date", color: Colors.white),
 
               SizedBox(height: 5),
-              CustomTextField(controller: expirydate, hintText: "dd/mm/yyyy"),
+              CustomTextField(controller: expirydate,
+                  onTap:(){
+                _selectDate(expirydate);
+                  },hintText: "dd/mm/yyyy"),
               SizedBox(height: 20),
               CustomButton(
                 text: 'Save Medicine',
@@ -86,7 +182,7 @@ class _AddmedicineState extends ConsumerState<Addmedicine> {
                       company.text.isEmpty ||
                       expirydate.text.isEmpty ||
                       sellingprice.text.isEmpty ||
-                      catergory.text.isEmpty) {
+                      catergory.text==null) {
                     ScaffoldMessenger.of(context).showSnackBar(
                       const SnackBar(content: Text("Please fill all fields")),
                     );
@@ -109,6 +205,11 @@ class _AddmedicineState extends ConsumerState<Addmedicine> {
                   await ref
                       .read(medicineProvider.notifier)
                       .addMedicine(medicine);
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text("Add medecine sucesfully")),
+
+                  );
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>MainNavigationScreen(initialIndex: 1,)));
                 },
               ),
               SizedBox(height: 50),
