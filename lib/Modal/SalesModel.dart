0d8dd8b0722ca59fb1d84b03c66391f954d64d40
razak <rm7740736@gmail.com>
2026-06-29 12:21:18 +0500

    done

--- lib/Modal/SalesModel.dart
+++ lib/Modal/SalesModel.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class SaleModel {
   final String id;
   final DateTime date;
@@ -11,10 +13,24 @@ class SaleModel {
     required this.items,
   });
 
+  factory SaleModel.fromMap(Map<String, dynamic> map) {
+    return SaleModel(
+      id: map["id"] ?? "",
+
+      date: (map["date"] as Timestamp).toDate(),
+
+      total: (map["total"] as num).toDouble(),
+
+      items: List<Map<String, dynamic>>.from(
+        map["items"] ?? [],
+      ),
+    );
+  }
+
   Map<String, dynamic> toMap() {
     return {
       "id": id,
-      "date": date,
+      "date": Timestamp.fromDate(date),
       "total": total,
       "items": items,
     };
